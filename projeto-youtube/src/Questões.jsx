b5f02956fd442ae69ebd2493269fb7e3faa69fa4
Daniel <danieldfl010492@gmail.com>
2024-08-26 15:30:45 -0300

    Quizz100%

--- projeto-youtube/src/Questões.jsx
+++ projeto-youtube/src/Questões.jsx
@@ -6,90 +6,127 @@ const Questões = ({ perguntas }) => {
     const [respostaIndex, setRespostasIndex] = useState(null);
     const [respostas, setRespostas] = useState(null);
     const [resultado, setResultado] = useState(resultadoInicialState);
-    const [mostreResultado, setMostreResultado] = useState(false)
+    const [mostreResultado, setMostreResultado] = useState(false);
+    const [nome, setNome] = useState('');
+    const [matricula, setMatricula] = useState('');
+    const [inicioQuiz, setInicioQuiz] = useState(false);
 
-    const {questão, escolhas, resposta}= perguntas[perguntaAtual];
+    const { questão, escolhas, resposta, explicacao } = perguntas[perguntaAtual];
 
-    const aoResponderClick = (respostas, index) => {
+    const aoResponderClick = (escolha, index) => {
+        if (respostaIndex !== null) return;
+
         setRespostasIndex(index);
-        if (respostas === resposta) {
+        if (escolha === resposta) {
             setRespostas(true);
-        }
-        else {
+        } else {
             setRespostas(false);
         }
-    }
+    };
 
     const onClickNext = () => {
         setRespostasIndex(null);
         setResultado((prev) => 
-        respostas
-        ? {
-            ...prev,
-            score: prev.score + 5,
-            respostasCertas: prev.respostasCertas + 1
-        } : {
-            ...prev,
-            respostasErradas: prev.respostasErradas + 1
-        });
+            respostas
+            ? {
+                ...prev,
+                score: prev.score + 5,
+                respostasCertas: prev.respostasCertas + 1,
+            } : {
+                ...prev,
+                respostasErradas: prev.respostasErradas + 1,
+            }
+        );
 
         if (perguntaAtual !== perguntas.length - 1) {
-            setPerguntaAtual((prev) => prev +1);
-        }
-        else {
+            setPerguntaAtual((prev) => prev + 1);
+        } else {
             setPerguntaAtual(0);
             setMostreResultado(true);
         }
-
     };
 
     const onTenteNovamente = () => {
         setResultado(resultadoInicialState);
         setMostreResultado(false);
+        setInicioQuiz(false);
+        setNome('');
+        setMatricula('');
     };
 
-    return <div className="containerPergunta">
+    const iniciarQuiz = () => {
+        if (nome && matricula) {
+            setInicioQuiz(true);
+        } else {
+            alert("Por favor, preencha seu nome e matrícula.");
+        }
+    };
 
-        {!mostreResultado ? (
-            <>
-            <span className="perguntasAtivas">{perguntaAtual + 1}</span>
-            <span className="perguntasTotais">/{perguntas.length}</span>
-            <h2>{questão}</h2>
-            <ul>
-                {escolhas.map((escolhas, index) => (
-                    <li    
-                    onClick={()=> aoResponderClick(escolhas, index)}
-                    key={escolhas}
-                    className={respostaIndex === index ? 'opção-selecionada' : null}
-                    >
-                        {escolhas}
-                        </li>
-                    ))}
-            </ul>
-            <div className="rodape">
-                <button onClick={onClickNext} disabled = {respostaIndex === null}>
-                    {perguntaAtual === perguntas.length - 1 ? "Finish" : "Next" }
-                </button>
-            </div>
-            </>
-        ) : (<div className="resultado">
-            <h3>Resultado:</h3>
-            <p>
-                Total de Questões: <span>{perguntas.length}</span>
-            </p>
-            <p>
-                Placar: <span>{resultado.score}</span>
-            </p>
-            <p>
-                Acertos: <span>{resultado.respostasCertas}</span>
-            </p>
-            <p>
-                Erros: <span>{resultado.respostasErradas}</span>
-            </p>
-            <button onClick={onTenteNovamente}>Tente Novamente</button>
-            </div>
-        )}
-        </div>;
+    return (
+        <div className="containerPergunta">
+            {!inicioQuiz ? (
+                <div className="inicio">
+                    <h2>Por favor, insira suas informações:</h2>
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                        className="input"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Matrícula"
+                        value={matricula}
+                        onChange={(e) => setMatricula(e.target.value)}
+                        className="input"
+                    />
+                    <button onClick={iniciarQuiz} className="start-button">Iniciar Quiz</button>
+                </div>
+            ) : !mostreResultado ? (
+                <>
+                    <span className="perguntasAtivas">{perguntaAtual + 1}</span>
+                    <span className="perguntasTotais">/{perguntas.length}</span>
+                    <h2>{questão}</h2>
+                    <ul>
+                        {escolhas.map((escolha, index) => (
+                            <li
+                                onClick={() => aoResponderClick(escolha, index)}
+                                key={escolha}
+                                className={
+                                    respostaIndex === index
+                                        ? respostas
+                                            ? 'opção-correta'
+                                            : 'opção-incorreta'
+                                        : null
+                                }
+                                style={{ pointerEvents: respostaIndex !== null ? 'none' : 'auto' }}
+                            >
+                                {escolha}
+                            </li>
+                        ))}
+                    </ul>
+                    {respostaIndex !== null && !respostas && (
+                        <p className="explicacao">{explicacao}</p>
+                    )}
+                    <div className="rodape">
+                        <button onClick={onClickNext} disabled={respostaIndex === null}>
+                            {perguntaAtual === perguntas.length - 1 ? "Finish" : "Next"}
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <div className="resultado">
+                    <h3>Resultado:</h3>
+                    <p>Total de Questões: <span>{perguntas.length}</span></p>
+                    <p>Placar: <span>{resultado.score}</span></p>
+                    <p>Acertos: <span>{resultado.respostasCertas}</span></p>
+                    <p>Erros: <span>{resultado.respostasErradas}</span></p>
+                    <button onClick={onTenteNovamente}>Tente Novamente</button>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Questões;
--- projeto-youtube/src/constantes.js
+++ projeto-youtube/src/constantes.js
@@ -1,63 +1,88 @@
 export const jsQuestões = {
-    perguntas: [
-      {
-        questão:
-          "Qual das alternativas a seguir descreve corretamente o React?",
-        escolhas: [
-          "O React é um framework de back-end JavaScript de código fechado, usado para gerenciar bancos de dados.",
-          "O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.",
-          "O React é uma ferramenta de design gráfico usada para criar logotipos e animações.",
-          "O React é um ambiente de desenvolvimento integrado (IDE) usado para criar aplicativos de desktop.",
-        ],
-        type: "MCQs",
-        resposta: "O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.",
-      },
-      {
-        questão: "Como muda a cor de um elemento em html?",
-        escolhas: [
-          "background-color",
-          "font-color",
-          "shadow-color",
-          "color",
-        ],
-        type: "MCQs",
-        resposta: "color",
-      },
-      {
-        questão: "O React foi criado por qual empresa?",
-        escolhas: [
-          "Amazon", 
-          "Apple", 
-          "Facebook", 
-          "Google"
-        ],
-        type: "MCQs",
-        resposta: "Facebook",
-      },
-      {
-        questão: "O que fez a função render em um componente de classe React?",
-        escolhas: [
-          "Atualiza o estado do componente", 
-          "Cria uma nova instância do componente", 
-          "Retorna o conteúdo JSX que será exibido na tela", 
-          "Adiciona estilos ao componente"
-        ],
-        type: "MCQs",
-        resposta: "Retorna o conteúdo JSX que será exibido na tela",
-      },
-      {
-        questão: "Qual a linguagem de programação usada pelo React?",
-        escolhas: [
-          "Python",
-          "JavaScript",
-          "C++",
-          "TypeScript",
-        ],
-        type: "MCQs",
-        resposta: "JavaScript",
-      },
-    ],
-  };
+  perguntas: [
+    {
+      questão:
+        "Qual das alternativas a seguir descreve corretamente o React?",
+      escolhas: [
+        "O React é um framework de back-end JavaScript de código fechado, usado para gerenciar bancos de dados.",
+        "O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.",
+        "O React é uma ferramenta de design gráfico usada para criar logotipos e animações.",
+        "O React é um ambiente de desenvolvimento integrado (IDE) usado para criar aplicativos de desktop.",
+      ],
+      type: "MCQs",
+      resposta: "O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.",
+      explicacao: "O React é uma biblioteca front-end JavaScript de código aberto que permite criar interfaces de usuário de forma eficiente e flexível em aplicações web."
+    },
+    {
+      questão: "Como muda a cor de um elemento em html?",
+      escolhas: [
+        "background-color",
+        "font-color",
+        "shadow-color",
+        "color",
+      ],
+      type: "MCQs",
+      resposta: "color",
+      explicacao: "A propriedade 'color' em CSS é usada para alterar a cor do texto em um elemento HTML."
+    },
+    {
+      questão: "O React foi criado por qual empresa?",
+      escolhas: [
+        "Amazon", 
+        "Apple", 
+        "Facebook", 
+        "Google"
+      ],
+      type: "MCQs",
+      resposta: "Facebook",
+      explicacao: "O React foi desenvolvido pelo Facebook e lançado em 2013 como uma ferramenta para construir interfaces de usuário."
+    },
+    {
+      questão: "O que faz a função render em um componente de classe React?",
+      escolhas: [
+        "Atualiza o estado do componente", 
+        "Cria uma nova instância do componente", 
+        "Retorna o conteúdo JSX que será exibido na tela", 
+        "Adiciona estilos ao componente"
+      ],
+      type: "MCQs",
+      resposta: "Retorna o conteúdo JSX que será exibido na tela",
+      explicacao: "A função render de um componente de classe React retorna o JSX que representa o conteúdo a ser exibido no DOM."
+    },
+    {
+      questão: "Qual a linguagem de programação usada pelo React?",
+      escolhas: [
+        "Python",
+        "JavaScript",
+        "C++",
+        "TypeScript",
+      ],
+      type: "MCQs",
+      resposta: "JavaScript",
+      explicacao: "O React é escrito em JavaScript, uma linguagem de programação amplamente usada para desenvolvimento web."
+    },
+    {
+      questão: "O React é uma biblioteca JavaScript usada para construir interfaces de usuário em aplicações web?",
+      escolhas: [
+        "Certo",
+        "Errado",
+      ],
+      type: "MCQs",
+      resposta: "Certo",
+      explicacao: "O React é uma biblioteca desenvolvida pelo Facebook que facilita a criação de interfaces de usuário, especialmente para aplicações web, permitindo a construção de componentes reutilizáveis e eficientes."
+    },
+    {
+      questão: "No React, componentes funcionais não podem usar o hook useState para gerenciar estados?",
+      escolhas: [
+        "Certo",
+        "Errado",
+      ],
+      type: "MCQs",
+      resposta: "Errado",
+      explicacao: "Com o React 16.8 e a introdução dos hooks, componentes funcionais agora podem usar useState para gerenciar estado, o que antes era possível apenas em componentes de classe."
+    },
+  ],
+};
 
 export const resultadoInicialState = {
     score: 0,
